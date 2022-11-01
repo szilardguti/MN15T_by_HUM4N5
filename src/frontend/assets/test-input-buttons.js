88a13a107e7e361bb24b64a70af5418b8b1e1909
Laci730 <nagylaci755@gmail.com>
2022-11-01 11:35:27 +0100

    fejlesztés - frontend - input gombok függvényei

---
+++ src/frontend/assets/test-input-buttons.js
@@ -0,0 +1,7 @@
+function inputValue(value) {
+    document.getElementById("chosen-value").innerHTML = `Válasz: ${value}`;
+}
+
+function deleteValue() {
+    document.getElementById("chosen-value").innerHTML = "Válasz:"
+}
