83151c88b1a3f4df639ea02a087391524e930acb
erdeiakos <eakos2001@gmail.com>
2022-11-04 15:52:58 +0100

    fejlesztés-frontend-dizájnolás

--- src/frontend/assets/test-input-buttons.js
+++ src/frontend/assets/test-input-buttons.js
@@ -1,3 +1,5 @@
+const { Tooltip } = require("chart.js");
+
 var isActive = false;
 
 function inputValue(value) {
@@ -23,7 +25,7 @@ function submitVote() {
         graph = document.c/* reateElement("div")
         graph.className = "graphs" */
 
-        div = document.createElement("div")
+        div = document.getElementById("div")
 
       /*   canvas = document.createElement("canvas")
         canvas.id = "myChart"
@@ -36,7 +38,7 @@ function submitVote() {
         next.type="button"
 
         /* rightside.appendChild(graph) */
-        rightside.appendChild(div)
+        /* rightside.appendChild(div) */
         div.appendChild(next)
        /*  graph.appendChild(canvas) */
 
@@ -50,17 +52,30 @@ function submitVote() {
                     label: 'Szavazatok eloszlása',
                     data: [12, 19, 3, 5, 2, 3],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
                     ]
                 }]
             },
             options:{
                 indexAxis: 'y',
+                padding:30,
+                plugins: {
+                    legend: {
+                        labels: {
+                            // This more specific font property overrides the global property
+                            font: {
+                                size: 14,
+                                weight: 5
+                            }
+                        }
+                    }
+                   
+                }
             }
         });
 
