9b734b75a541ed97e20d337932b3ed866419edc6
erdeiakos <eakos2001@gmail.com>
2022-11-07 17:33:46 +0100

    feljelsztés-frontend-randomkép generálás, modulos hiba létrehozása(ToDO javítani xd))

--- src/frontend/assets/randomPic.js
+++ src/frontend/assets/randomPic.js
@@ -1,3 +1,39 @@
-function randomPic() {
-    //Random kép id generálása és elkérése az adatbázisból
+export function randomPic(solved, testerId, list_of_done, callback) {
+    console.log('asd')
+    let randomn = Math.floor(Math.random() * 101);
+    if(randomn<solved)
+    {   
+        console.log('asd3')
+        fetch('http://localhost:3030/tester/' + testerId + '/highdeviation')
+        .then (response => response.json())
+        .then(res_json => {
+            let res_data = res_json['0'];
+            console.log('asd2')
+            if(res_data.Deviation == 0 )
+                {
+                    returnAnyRandomPic(list_of_done,callback)
+                }
+                else{
+                    callback(res_data)
+                }
+        });
+    }
+    else{
+        returnAnyRandomPic(list_of_done,callback)
+    }
+}
+function returnAnyRandomPic(list_of_done, callback)
+{
+    console.log("xd")
+    let randomn = 0;
+    do{
+        randomn = Math.floor(Math.random() * 4000)+1
+    }while (list_of_done.includes(randomn));
+    fetch('http://localhost:3030/images/' + randomn)
+        .then (response => response.json())
+        .then(res_json => {
+            let res_data = res_json['0'];
+            callback(res_data);
+        });
+    console.log(randomn)
 }
