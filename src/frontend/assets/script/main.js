7c28762a96e487b29fceadce3b5ff13e767823e1
Laci730 <nagylaci755@gmail.com>
2022-11-09 12:28:46 +0100

    deleteValue módosítás

--- src/frontend/assets/script/main.js
+++ src/frontend/assets/script/main.js
@@ -1,10 +1,8 @@
 import { randomPic } from "./randomPic.js"
-import { deleteValue, submitVote } from './test-input-buttons.js'
+//import { submitVote } from './test-input-buttons.js'
 
 // Initialize random picture when page is loaded
 randomPic(100,1,[],(res)=>{console.log(res)})
 
 // Bind imports
-window.inputValue = inputValue;
-window.deleteValue = deleteValue;
-window.submitVote = submitVote;
+//window.submitVote = submitVote;
