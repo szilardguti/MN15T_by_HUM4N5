c30f15cf2233d8cac79f292986cae661054c8388
Laci730 <nagylaci755@gmail.com>
2022-11-22 16:43:49 +0100

    fejlesztés - frontend - login oldal css finomítás és figyelmeztető szöveg üres mezők esetén

--- src/frontend/assets/script/login.js
+++ src/frontend/assets/script/login.js
@@ -1,40 +1,49 @@
-function register()
-{
-    let age = document.getElementById("age")
-    let bender = document.getElementById("choose-gender")
-    let school = document.getElementById("suli")
-    let gender = getData(bender) 
+function register() {
+  let age = document.getElementById("age");
+  let bender = document.getElementById("choose-gender");
+  let school = document.getElementById("suli");
+  let gender = getData(bender);
 
-    fetch('http://localhost:3030/tester',{
-        method:'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body:JSON.stringify({
-            "Age":age.value,
-            "Gender":gender,
-            "Studies":school.value
-        })
+  console.log(age.value);
+  console.log(school.value);
+  console.log(gender);
+
+  if (gender == undefined || school.value == "Kérem válasszon" || age.value == "") {
+    let errorMsg = document.getElementById("error-text");
+    errorMsg.innerHTML = "A továbblépéshez töltsön ki minden mezőt";
+    errorMsg.style.fontSize = "14px";
+  }
+  else {
+    fetch('http://localhost:3030/tester', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        "Age": age.value,
+        "Gender": gender,
+        "Studies": school.value
+      })
 
 
     })
-    .then(res => res.json())
-    .then(res => {
+      .then(res => res.json())
+      .then(res => {
         setUser(res);
-        console.log(res)})
+        console.log(res)
+      })
+  }
 }
 
 function getData(form) {
-    var formData = new FormData(form);
+  var formData = new FormData(form);
 
-    for (var pair of formData.entries()) {
-      return pair[1]
-    }
+  for (var pair of formData.entries()) {
+    return pair[1]
   }
+}
 
-  function setUser(userID)
-  {
-    sessionStorage.setItem("UserID", userID)
-    window.location.href = "/src/frontend/test.html";
-  }
-        
+function setUser(userID) {
+  sessionStorage.setItem("UserID", userID)
+  window.location.href = "/src/frontend/test.html";
+}
