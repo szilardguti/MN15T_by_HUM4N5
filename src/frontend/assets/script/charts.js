9d6d25e185f5077353019e8cd4e226c3c34f0e15
erdeiakos <eakos2001@gmail.com>
2022-11-27 16:57:45 +0100

    feljelsztés-frontend-összesített statisztika oldal chart megjelenítések

--- src/frontend/assets/script/charts.js
+++ src/frontend/assets/script/charts.js
@@ -13,34 +13,36 @@ function getstatfnreg()
     fetch('http://localhost:3030/stat/gender/testers')
         .then(response => response.json())
         .then(res_json => {
-            let res_data = res_json['0'];
-            generatestatfnreg(res_data);
+            generatestatfnreg(res_json);
         })
 }
+getstatfnreg()
+
 function getstatbystudiesdone()
 {
     fetch('http://localhost:3030/stat/studies/tests')
         .then(response => response.json())
         .then(res_json => {
-            let res_data = res_json['0'];
-            generatestatbystudiesdone(res_data);
+            generatestatbystudiesdone(res_json);
         })
 }
+getstatbystudiesdone()
+
 function getstatbystudiesreg()
 {
     fetch('http://localhost:3030/stat/studies/testers')
         .then(response => response.json())
         .then(res_json => {
-            let res_data = res_json['0'];
-            generatestatbystudiesreg(res_data);
+            generatestatbystudiesreg(res_json);
         })
 }
+getstatbystudiesreg()
+
 function getstatbyages()
 {
     fetch('http://localhost:3030/stat/ages')
         .then(response => response.json())
         .then(res_json => {
-            console.log(res_json)
             generatestatbyages(res_json);
         })
 }
@@ -51,8 +53,7 @@ function getstattop5()
     fetch('http://localhost:3030/stat/topDev')
         .then(response => response.json())
         .then(res_json => {
-            let res_data = res_json['0'];
-            generatestattop5(res_data);
+            generatestattop5(res_json);
         })
 }
 function getstattop10()
@@ -60,10 +61,11 @@ function getstattop10()
     fetch('http://localhost:3030/stat/topStreak')
         .then(response => response.json())
         .then(res_json => {
-            let res_data = res_json['0'];
-            generatestattop10(res_data);
+            generatestattop10(res_json);
         })
 }
+getstattop10()
+
 
 function generatestatfndone(givenData)
 {
@@ -77,7 +79,7 @@ function generatestatfndone(givenData)
         data: {
           labels: genderLabel,
           datasets: [{
-            backgroundColor: ['blue', 'red'],
+            backgroundColor: ['green', 'purple'],
             data: chartValues
           }]
         },
@@ -124,4 +126,129 @@ function generatestatbyages(givenData)
            maintainAspectRatio: false
         }
       });
+}
+function generatestatfnreg(givenData)
+{
+    const ctx = document.getElementById('gender-sum');
+
+    let genderLabel = ['Female', 'Male'];
+    let chartValues = [givenData['0']['TesterCount'], givenData['1']['TesterCount']];
+
+    new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: genderLabel,
+          datasets: [{
+            backgroundColor: ['green', 'purple'],
+            data: chartValues
+          }]
+        },
+        options: {
+          title: {
+            display: true,
+            text: "TODO"
+          },
+           responsive:true,
+           maintainAspectRatio: false
+        }
+      });
+}
+
+function generatestatbystudiesdone(givenData)
+{
+    const ctx = document.getElementById('studies');
+
+    let studiesLabel = [];
+    let chartValues = [];
+
+    givenData.forEach(element => {
+        studiesLabel.push(element['Studies'])
+        chartValues.push(element['TestCount'])
+    });
+
+
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: studiesLabel,
+          datasets: [{
+            label: 'Number of tests done by different study levels',
+            data: chartValues
+          }]
+        },
+        options: {
+          title: {
+            display: false,
+            text: "TODO"
+          },
+           responsive:true,
+           maintainAspectRatio: false
+        }
+      });
+}
+
+function generatestatbystudiesreg(givenData)
+{
+    const ctx = document.getElementById('studies2');
+
+    let studiesLabel = [];
+    let chartValues = [];
+
+    givenData.forEach(element => {
+        studiesLabel.push(element['Studies'])
+        chartValues.push(element['TesterCount'])
+    });
+
+
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: studiesLabel,
+          datasets: [{
+            label: 'Number of testers by different study levels',
+            data: chartValues
+          }]
+        },
+        options: {
+          title: {
+            display: false,
+            text: "TODO"
+          },
+           responsive:true,
+           maintainAspectRatio: false
+        }
+      });
+}
+
+function generatestattop10(givenData)
+{
+    var xValues = [1, 2 ,3 ,4, 5, 6, 7, 8, 9, 10 ];
+    var yValues = [];
+
+    const ctx = document.getElementById('top-ten-streak');
+
+    givenData.forEach(element => {
+        yValues.push(element['testCount'])
+    });
+
+    new Chart(ctx, {
+            type: "line",
+            data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: "rgba(0,0,0,1.0)",
+            borderColor: "rgba(0,0,0,0.1)",
+            data: yValues,
+            label: 'Top 10 helyezett',
+        }]
+        },
+  options: {
+            title: {
+              display: false,
+              text: "TODO"
+            },
+             responsive:true,
+             maintainAspectRatio: false
+          }
+        });
 }
